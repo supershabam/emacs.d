0d2f5060ca0f56c2e1ac4ac32e109fc984dcb5e8
Ian Hansen <ihansen@digitalocean.com>
2015-01-13 15:08:20 -0500

    adds gofmt on save

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,3 +1,9 @@
 (require-package 'go-mode)
 
+(defun my-go-mode-hook ()
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (local-set-key (kbd "M-.") 'godef-jump))
+
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
 (provide 'init-go)
